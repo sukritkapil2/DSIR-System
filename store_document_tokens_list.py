cf5b4506302b987c2d6e2405206a1445605ce98a
Mridul Kumar Rai <mridulkumarrai@users.noreply.github.com>
2020-10-18 17:43:22 +0530

    Decription indexed
    IR sys checked, shortDesc  tokens are now being retreived as well

--- store_document_tokens_list.py
+++ store_document_tokens_list.py
@@ -27,27 +27,26 @@ def create_document_tokens_list():
     """
     Function for creating document_tokens_list and then storing in json file for further usage
     """
-    count = 0
     for file in docFiles:
         document = dict()
         with open("./jsonnn/" + str(file) + ".json", encoding='utf8') as json_data:
             document = json.load(json_data)
 
-        count += 1
         words = str(document["title"])
         for author in document["authors"]:
             words += str(" " + author)
 
         for category in document["categories"]:
             words += str(" " + category)
+        
+        for des in document["desc"]:
+            words += str(des)
 
-        print(count)
         temp_doc_tokens = nltk.word_tokenize(words)
-        # print(temp_doc_tokens)
         temp_doc_tokens = [w.lower() for w in temp_doc_tokens]
         temp_doc_tokens = [snowball_stemmer.stem(token) for token in temp_doc_tokens]
         temp_doc_tokens = [token for token in temp_doc_tokens if token not in nltk.corpus.stopwords.words('english')]
-        # print(temp_doc_tokens)
+
         document_tokens_list.append(temp_doc_tokens)
 
     # storing in json file
